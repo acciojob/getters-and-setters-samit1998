e4d363ac8e3375c255678dfe81c85db06a48d2ba
SAMIT KUMAR BARIK <126311037+samit1998@users.noreply.github.com>
2023-07-22 12:31:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ class Teacher extends Person {
   }
 }
 const student1 = new Student("Alice", 20);
+console.log(student1.name); // Output: "Alice"
+student1.study(); // Output: "Alice is studying"
+
+const teacher1 = new Teacher("Bob", 35);
+teacher1.teach(); // Output: "Bob is teaching"
+
+// Changing age using the setter
+teacher1.age = 40;
 
 // Do not change the code below this line
 window.Person = Person;
